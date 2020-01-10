b4339748f9e6c9944fb1688c85339d7e3c78b124
Wendy Wong <wwong@ayra.fios-router.home>
2020-01-10 09:31:12 -0500

    fixed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Wendy Wong's Personal Page",
+    author: "Wendy Wong",
+    description: "Wendy Wong's homepage,  based on Dimension by HTML5 UP"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
