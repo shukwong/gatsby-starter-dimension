b7e75839365cca506f8e6747a57834d56bf927d1
Wendy Wong <wwong@ayra.fios-router.home>
2020-01-09 22:59:50 -0500

    added stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,29 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+         <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/wendyWongSW"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/wendyswwong/" className="icon fa-linkedin">
+                <span className="label">Linkedin</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/shukwong"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+          </ul>
         <p className="copyright">&copy; Wendy Wong 2020. Theme designed by <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import wendyCV from '../images/WendyWongCV.png'
 
 class Main extends React.Component {
   render() {
@@ -29,6 +30,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">About Me</h2>
+
           <span className="image main">
             <img src={pic01} alt="" />
           </span>
@@ -51,23 +53,10 @@ class Main extends React.Component {
         >
           <h2 className="major">My CV</h2>
           <span className="image main">
-            <img src={pic02} alt="" />
+            <img src={wendyCV} alt="" />
           </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p>
+         
+      
           {close}
         </article>
 
@@ -83,7 +72,46 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            A full list of my publications can be found at Google Scholar.
+            A full list of my publications can be found at <a href="https://scholar.google.com/citations?user=q91I6UYAAAAJ&hl=en">Google Scholar</a>. Here are a some of the highlighted ones:
+          </p>
+          <p>
+(2018)	Goldmann JM, Seplyarskiy VB, Wong WSW*, Vilboux T, Neerincx PB, Bodian DL, Solomon BD, Veltman JA, Deeken JF, Gilissen C, Niederhuber JE. Germline de novo mutation clusters arise during oocyte aging in genomic regions with high double-strand-break incidence. Nature Genetics. 2018 Apr;50(4):487-492. 
+</p>
+<p>
+(2016)	Jakob M Goldmann, Wendy SW Wong*, Michele Pinelli, et al. Parent-of-origin-specific signatures of de novo mutations. Nature Genetics. volume 48, pages 935–939.
+</p>
+<p>
+(2016)	Wendy SW Wong, Benjamin D Solomon, Dale L Bodian, et al. New observations on maternal age effect on germline de novo mutations. Nature Communications, 2016 Jan 19;7:10486.
+</p>
+<p>
+(2012)	Kong A, Frigge ML, Masson G, Besenbacher S, Sulem P, Magnusson G, Gudjonsson 
+	SA, Sigurdsson A, Jonasdottir A, Jonasdottir A, Wong WS, et al. Rate of de novo mutations and the importance of father's age to disease risk. Nature. Aug 23;488(7412):471-5.
+  </p>
+  <p>
+(2012)  	Saunders CT, Wong WS, Swamy S, Becq J, Murray LJ, Cheetham RK. Strelka:
+	accurate somatic small-variant calling from sequenced tumor-normal sample pairs.
+	Bioinformatics. Jul 15;28(14):1811-7.
+  </p>
+  <p>
+(2007)	Wendy S.W. Wong and Rasmus Nielsen. Finding Cis-Regulatory Modules in
+	Drosophila using Phylogenetic Hidden Markov Models. Bioinformatics. Aug	15:23(16):2031-7.  
+  </p>
+  <p>
+(2004)	Ziheng Yang, Wendy S. W. Wong and Rasmus Nielsen. Bayes Empirical Bayes 
+	Inference of Amino Acid Sites under Positive Selection. Molecular Biology and 
+	Evolution 22: 1107-1118.
+  </p>
+  <p>
+(2004)	Wendy S. W. Wong, Ziheng Yang, Nick Goldman and Rasmus Nielsen. Accuracy 
+	and power of statistical methods for detecting adaptive evolution in protein coding 
+	sequences and for identifying positively selected sites. Genetics 168: 1041-1051.
+  </p>
+  <p>
+(2004)	Wendy S. W. Wong and Rasmus Nielsen. Detecting selection in noncoding regions 
+	of nucleotide sequences. Genetics 167: 949-958.
+  </p>
+  <p>
+*joint first author  
           </p>
           {close}
         </article>
@@ -96,7 +124,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form action="https://formspree.io/xyyeyepq" method="POST">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
